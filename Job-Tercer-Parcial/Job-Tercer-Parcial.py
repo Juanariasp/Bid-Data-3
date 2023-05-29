34f7c43ded669b9ca8827d24ad92214339711364
Juan Esteban Arias <79543271+Juanariasp@users.noreply.github.com>
2023-05-29 12:33:54 -0500

    Synchronizing job Job-Tercer-Parcial from AWS Glue at 2023/05/29 17:33:54

--- Job-Tercer-Parcial/Job-Tercer-Parcial.py
+++ Job-Tercer-Parcial/Job-Tercer-Parcial.py
@@ -1,15 +1,19 @@
 spark
+# Se importan las librerias necesarias 
+
 import datetime
 from pyspark.ml import Pipeline
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StringType
 from pyspark.sql.functions import udf, concat_ws
 from pyspark.ml.feature import RegexTokenizer, StopWordsRemover, CountVectorizer, IDF
+# Se lee la informacion del bucket
 df = spark.read.csv("s3://segundo-1001168599/headlines/final/", header=True, inferSchema=True)
 # Mostramos los primeros diez registros
 df.show(10)
 # Mostramos los tipos de datos de las columnas
 df.printSchema()
+# Eliminamos los valores nulos
 df = df.na.drop()
 regex_tokenizer_1 = RegexTokenizer(inputCol="Nombre", outputCol="nombre_tk", pattern="\\W")
 regex_tokenizer_2 = RegexTokenizer(inputCol=" Categoria", outputCol="categoria_tk", pattern="\\W")
@@ -27,6 +31,7 @@ idf_1 = IDF(inputCol="nombre_features", outputCol="features_nombre")
 idf_2 = IDF(inputCol="categoria_features", outputCol="features_categoria")
 idf_3 = IDF(inputCol="link_features", outputCol="features_link")
 idf_4 = IDF(inputCol="periodico_features", outputCol="features_periodico")
+# Creamos el pipeline de procesamiento para la transformacion de los datos
 pipeline = Pipeline(stages=[regex_tokenizer_1, regex_tokenizer_2, regex_tokenizer_3, regex_tokenizer_4,
                             stopwords_remover_1, stopwords_remover_2, stopwords_remover_3, stopwords_remover_4,
                             count_vectorizer_1, count_vectorizer_2, count_vectorizer_3, count_vectorizer_4,
@@ -34,7 +39,7 @@ pipeline = Pipeline(stages=[regex_tokenizer_1, regex_tokenizer_2, regex_tokenize
 # Aplica el pipeline a los datos
 model = pipeline.fit(df)
 new_data = model.transform(df)
-# Convierte la columna "features" en una cadena de texto
+# Convierte las columnas en una cadena de texto
 array_to_string_udf = udf(lambda arr: ' '.join(str(x) for x in arr), StringType())
 new_data = new_data.withColumn("nombre_features", array_to_string_udf(new_data["features_nombre"]))
 new_data = new_data.withColumn("categoria_features", array_to_string_udf(new_data["features_categoria"]))
